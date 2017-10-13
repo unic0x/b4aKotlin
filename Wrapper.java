102b70ef8d6255a9ffbd121907d3ddea0939dccf
J <askquickly@users.noreply.github.com>
2017-10-13 13:49:15 +0100

    Create Wrapper.java

---
+++ Wrapper.java
@@ -0,0 +1,28 @@
+package com.Kotlin;
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.BA.*;
+
+@Version(1.0f)
+@ShortName("Kotlin")
+@DependsOn(values={"example"})
+
+	public int randomnumber(int x,int z) {
+		BA.Log("Kotlin fun to from called");
+	new demo.b4akotlin();
+	return demo.DemoUtils.rand(x , z);
+}
+
+	public static int summ(int[] a) {
+		BA.Log("Kotlin fun int array called");
+	new demo.b4akotlin();
+	return demo.DemoUtils.sum(a);
+}
+
+	public static int findpairs(int[] a) {
+		BA.Log("Kotlin fun pairs called");
+	new demo.b4akotlin();
+	return demo.DemoUtils.findPairless(a);
+}
+
+}
