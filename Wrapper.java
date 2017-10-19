8ccfdad93eec2bad29718a3d554d598ab3fd45b4
J <askquickly@users.noreply.github.com>
2017-10-19 04:55:22 +0100

    Typo

--- Wrapper.java
+++ Wrapper.java
@@ -6,23 +6,22 @@ import anywheresoftware.b4a.BA.*;
 @Version(1.0f)
 @ShortName("Kotlin")
 @DependsOn(values={"example"})
-
+public class Kotlin {
 	public int randomnumber(int x,int z) {
-		BA.Log("Kotlin fun to from called");
+	BA.Log("Kotlin fun to from called");
 	new demo.b4akotlin();
 	return demo.DemoUtils.rand(x , z);
-}
+	}
 
 	public static int summ(int[] a) {
 		BA.Log("Kotlin fun int array called");
 	new demo.b4akotlin();
 	return demo.DemoUtils.sum(a);
-}
+	}
 
 	public static int findpairs(int[] a) {
 		BA.Log("Kotlin fun pairs called");
 	new demo.b4akotlin();
 	return demo.DemoUtils.findPairless(a);
-}
-
+	}
 }
